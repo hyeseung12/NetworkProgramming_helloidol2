f6aa986509e468ca34c7532c7e0d396cc2efe381
MsEmily1020 <peace10200khs@gmail.com>
2024-05-13 12:17:28 +0900

    views CharacterListView

--- 여자친구/views.py
+++ 여자친구/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from 여자친구.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # character_list = Character.objects.all()  # DB에 모델 데이터 다 가져오기
+    # return render(request, '여자친구/character_list.html', context={'character_list': character_list})
+    # 모델_list.html에 모델_list라는 키로 DB에서 가져온 데이터 넣어서 render 하자
